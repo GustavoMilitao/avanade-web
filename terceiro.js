4d88ef94c7a4666e17a64623d2307aa2ec349219
Gustavo Militão <gustavo.h.militao@gmail.com>
2022-05-31 19:54:55 -0300

    refactor

--- terceiro.js
+++ terceiro.js
@@ -1,10 +1,10 @@
 // alert('foi importado o arquivo terceiro.js');
 
-function onClickLoginButton() {
-    var usuario = document.getElementById('usuario').value;
-    var senha = document.getElementById('senha').value;
-    alert(`o usuário digitado é ${usuario} e a senha é ${senha}`);
-}
+// function onClickLoginButton() {
+//     var usuario = document.getElementById('usuario').value;
+//     var senha = document.getElementById('senha').value;
+//     alert(`o usuário digitado é ${usuario} e a senha é ${senha}`);
+// }
 
 
 function obterPokemons() {
@@ -12,20 +12,21 @@ function obterPokemons() {
     xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon');
     xhr.onload = function () {
         var lista = JSON.parse(xhr.response).results.map((pokemon) => { return pokemon.name });
-        var xhr2 = new XMLHttpRequest();
-        xhr2.onload = function () {
-            console.log(JSON.parse(xhr2.response).sprites.front_default);
-            document.getElementById('pokemons').innerHTML += `<img src="${JSON.parse(xhr2.response).sprites.front_default}" />`
-        }
-        lista.forEach((nomePokemon, i) => {
-            console.log(i);
-            setTimeout(() => {
-                xhr2.open('GET', `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
-                xhr2.send();
-            }, 1000*i);
-        })
-        // document.getElementById('pokemons').innerText = lista;
+        pegarImagensPokemon(lista);
     };
     xhr.send();
 
+}
+
+function pegarImagensPokemon(lista) {
+    var xhr2 = new XMLHttpRequest();
+    xhr2.onload = function () {
+        document.getElementById('pokemons').innerHTML += `<img src="${JSON.parse(xhr2.response).sprites.front_default}" />`;
+    };
+    lista.forEach((nomePokemon, i) => {
+        setTimeout(() => {
+            xhr2.open('GET', `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
+            xhr2.send();
+        }, 1000 * i);
+    });
 }
