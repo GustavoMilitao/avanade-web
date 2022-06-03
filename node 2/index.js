7da69532184d8452a1a2e7d8c66f810b4ee8b7e6
Gustavo Militão <gustavo.h.militao@gmail.com>
2022-06-03 19:51:17 -0300

    validação de id, delete e get por id

--- node 2/index.js
+++ node 2/index.js
@@ -5,20 +5,53 @@ const app = express();
 app.use(bodyParser.json());
 
 
-const lista = [];
+var lista = [];
 
-function getObj(req, res) {
+function getPokemon(req, res) {
     res.send(lista);
 }
 
-function criandoObj(req, res) {
-    const obj = req.body;
-    lista.push(obj);
-    res.send('inserido com sucesso');
+function criandoPokemon(req, res) {
+    const pokemon = req.body;
+    const pokemonPesquisado = lista.find((pokemonNaLista) => pokemonNaLista.id === pokemon.id);
+    if (!pokemonPesquisado) {
+        lista.push(pokemon);
+        res.send('inserido com sucesso');
+    } else {
+        res.send(`não é possível inserir pois já existe um pokemon com o id ${pokemon.id}`);
+    }
 }
 
-app.get('/obj', getObj);
-app.post('/criando', criandoObj);
+function getPokemonById(req, res) {
+    const pokemonId = req.params.id;
+    const pokemonPesquisado = lista.find((pokemonNaLista) => pokemonNaLista.id == pokemonId);
+    if(!pokemonPesquisado){
+        res.send(`não existe um pokemón com o id ${pokemonId}`)
+    } else {
+        res.send(pokemonPesquisado);
+    }
+}
+
+function deletePokemon(req, res) {
+    const pokemonId = req.params.id;
+    lista = lista.filter((pokemon) => pokemon.id != pokemonId);
+    res.send(`pokemon de id ${pokemonId} foi removido com sucesso!`);
+}
+
+// Implemente uma verificação de id na rota POST /pokemon para NÃO inserir se já existir um pokemón 
+// na lista (utilizar a função find para obter pelo id, pesquisar no google caso tenha dúvidas).
+
+// Crie no projeto do pokemón a rota GET /pokemon/:id onde o id é o req.params.id e use a função 
+// find da lista para obter o objeto correspondente ao id.
+
+// Crie no projeto do pokemón a rota DELETE /pokemon/:id onde o id é o req.params.id e utilize a função 
+// filter da lista para atualizar a lista somente com os ids que NÃO são o id passado como 
+// parâmetro (se tiver dúvidas pesquisar no google como remover um objeto de uma lista de objetos).
+
+app.get('/pokemon', getPokemon);
+app.get('/pokemon/:id', getPokemonById);
+app.post('/pokemon', criandoPokemon);
+app.delete('/pokemon/:id', deletePokemon);
 
 
 app.listen(3000);
